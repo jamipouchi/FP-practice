8089659629755ac09ccc81131e8189fcf3570a3f
jamipouchi <miquelpuigturon@gmail.com>
2023-01-05 16:00:02 +0100

    fixed haskell... :)

--- 452-findMinArrowShots/findMinArrowShots.hs
+++ 452-findMinArrowShots/findMinArrowShots.hs
@@ -1,17 +1,15 @@
 import Data.List (sort)
 
 findMinArrowShots :: [[Int]] -> Int
-findMinArrowShots = helper . (pure . last . head) =<< sort
+findMinArrowShots = (countArrows . (last . head) =<< id) . sort
   where
-    helper :: Maybe Int -> [[Int]] -> Int
-    helper Nothing = const 0
-    helper (Just lastPoint) = (+ 1) . (helper . maybeGetLast =<< dropWhile ((>=) lastPoint . head))
-    maybeGetLast :: [[Int]] -> Maybe Int
-    maybeGetLast [] = Nothing
-    maybeGetLast (fst : rest) = pure $ last fst
+    countArrows :: Int -> [[Int]] -> Int
+    countArrows _ [] = 1
+    countArrows lastPoint (x : xs) = if head x > lastPoint then 1 + countArrows (last x) xs else countArrows (min lastPoint (last x)) xs
 
 main :: IO ()
 main = do
   print $ findMinArrowShots [[10, 16], [2, 8], [1, 6], [7, 12]]
   print $ findMinArrowShots [[1, 2], [3, 4], [5, 6], [7, 8]]
   print $ findMinArrowShots [[1, 2], [2, 3], [3, 4], [4, 5]]
+  print $ findMinArrowShots [[9, 12], [1, 10], [4, 11], [8, 12], [3, 9], [6, 9], [6, 7]]
