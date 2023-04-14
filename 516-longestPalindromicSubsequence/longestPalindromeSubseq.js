eb0ca587e9961df2fd0d2be0ab45401048780e31
jamipouchi <miquelpuigturon@gmail.com>
2023-04-14 09:41:24 +0200

    daily problem ts

---
+++ 516-longestPalindromicSubsequence/longestPalindromeSubseq.js
@@ -0,0 +1,30 @@
+"use strict";
+function longestPalindromeSubseq(s) {
+    const n = s.length;
+    let dp = new Array(n).fill(0);
+    let dpPrev = new Array(n).fill(0);
+    for (let i = n - 1; i >= 0; i--) { // We start filling from bottom right
+        dp[i] = 1;
+        for (let j = i + 1; j < n; j++) {
+            if (s[i] === s[j]) {
+                dp[j] = dpPrev[j - 1] + 2;
+            }
+            else {
+                dp[j] = Math.max(dpPrev[j], dp[j - 1]);
+            }
+        }
+        dpPrev = [...dp];
+    }
+    return dp[n - 1];
+}
+;
+/* x is base case l = 1
+x7890
+ x456
+  x23
+   x1
+    x
+*/
+// test
+console.log(longestPalindromeSubseq('bbbab')); // 4
+console.log(longestPalindromeSubseq('cbbd')); // 2
---
+++ 516-longestPalindromicSubsequence/longestPalindromeSubseq.ts
@@ -0,0 +1,30 @@
+function longestPalindromeSubseq(s: string): number {
+    const n = s.length;
+    let dp: number[] = new Array(n).fill(0);
+    let dpPrev: number[] = new Array(n).fill(0);
+    for (let i = n - 1; i >= 0; i--) { // We start filling from bottom right
+        dp[i] = 1;
+        for (let j = i + 1; j < n; j++) {
+            if (s[i] === s[j]) {
+                dp[j] = dpPrev[j - 1] + 2;
+            } else {
+                dp[j] = Math.max(dpPrev[j], dp[j - 1]);
+            }
+        }
+        dpPrev = [...dp];
+    }
+    return dp[n - 1];
+};
+
+/* x is base case l = 1, empty spaces contain 0
+x7890
+ x456
+  x23
+   x1
+    x
+*/
+
+
+// test
+console.log(longestPalindromeSubseq('bbbab')); // 4
+console.log(longestPalindromeSubseq('cbbd')); // 2
