c311884aa87b8aed7864719436a0b77f968d029d
jamipouchi <miquelpuigturon@gmail.com>
2023-07-14 21:41:42 +0200

    Remove final';'

--- 207-courseSchedule/canFinish.go
+++ 207-courseSchedule/canFinish.go
@@ -4,8 +4,8 @@ func canFinish(numCourses int, prerequisites [][]int) bool {
 	blocksCourses := make([][]int, numCourses)
 	blockedBy := make([]int, numCourses)
 	for _, pair := range prerequisites {
-		blockedBy[pair[0]]++;
-		blocksCourses[pair[1]] = append(blocksCourses[pair[1]], pair[0]);
+		blockedBy[pair[0]]++
+		blocksCourses[pair[1]] = append(blocksCourses[pair[1]], pair[0])
 	}
 	Q := []int{}
 	for course := 0; course < numCourses; course++ {
@@ -15,7 +15,7 @@ func canFinish(numCourses int, prerequisites [][]int) bool {
 	}
 	coursesScheduled := 0
 	for len(Q) > 0 {
-		course := Q[0];
+		course := Q[0]
 		Q = Q[1:]
 		coursesScheduled++
 		for _, blockedCourse := range blocksCourses[course] {
@@ -23,11 +23,6 @@ func canFinish(numCourses int, prerequisites [][]int) bool {
 			if blockedBy[blockedCourse] == 0 {
 				Q = append(Q, blockedCourse)
 			}
-		}
-	}
-	for _, numBlocks := range blockedBy {
-		if numBlocks > 0 {
-			return false
 		}
 	}
 	return coursesScheduled == numCourses
